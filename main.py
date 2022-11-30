9238410d728a8e2f81e53d188e4a0be7e27d9491
pranjalcool9897 <pranjalkulshresth9897@gmail.com>
2022-11-30 18:47:28 +0530

    making frames

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import customtkinter as ctk
 
 root=ctk.CTk()
 root.geometry("500x500+200+10")
-ctk.set_appearance_mode("light")
+ctk.set_appearance_mode("dark")
 root.title("Path Finder")
+root.minsize(200,200)
+##========================================FRAMES====================================
 
+F1=ctk.CTkFrame(root,borderwidth=5,bg_color="#547980")
+F1.pack(fill="y",side="left")
+F2=ctk.CTkFrame(root,borderwidth=5,bg_color="lightgreen",width=1180)
+F2.pack(fill="both",side="left")
 root.mainloop()
